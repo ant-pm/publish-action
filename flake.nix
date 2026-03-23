d4e8179fb985215f57b23bc322737b55e99f34d2
Vincent <vincent.mick.gustafsson@gmail.com>
2026-03-23 21:08:13 +0100

    feat: initial publish action

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in
+      {
+        devShells.default = pkgs.mkShell {
+          shellHook = "export NIX_SHELL_NAME='publish-action'";
+          nativeBuildInputs = with pkgs; [
+            nodejs_24
+            nodePackages.typescript
+          ];
+        };
+      }
+    );
+}
